7e911c5e09f198bf56cf69921a1f461aa959f0c3
cs586@njit.edu <66033141+Cs586@users.noreply.github.com>
2023-08-20 08:06:49 -0400

    update app

---
+++ badbank/src/data/user.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    userPic: "/images/Avatar0.png",
+    name: "Stanley",
+    email: "ssuntarsi@gmail.com",
+    password: "secret",
+    balance: 100,
+    transactionHistory: [],
+  },
+];
+
+export default users;
